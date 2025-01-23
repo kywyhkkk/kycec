6c7391adbe179d9d92c5f626705fedb524411fac
kywyhkkk <120381615+kywyhkkk@users.noreply.github.com>
2025-01-23 23:47:26 +0800

    Add files via upload

---
+++ test/java/org/sde/cec/CommonExperChainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sde.cec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonExperChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
